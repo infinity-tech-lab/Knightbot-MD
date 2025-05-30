71f9c4c0e9f185344ee96f21490b3a3ec6074abb
infinity-tech-lab <panashemachinya12@gmail.com>
2025-05-30 15:08:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Ethan'
+  botOwner: 'Ethan',//your name
   ownerNumber: '263773352681'
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
